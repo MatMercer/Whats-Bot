4cbacb79052f02382da1c15c7700d471caf242b8
l3399l <m3rc3r99@gmail.com>
2015-11-24 21:38:51 -0200

    Add say cmd and parseCmd
    Now it works.. a little

--- Whats-Bot.js
+++ Whats-Bot.js
@@ -13,30 +13,42 @@
 
 var lastText;
 var msgText;
+var args;
 
-function cmd (nm, syntax, desc) {
+function cmd(nm, syntax, desc) {
 	this.nm = nm;
 	this.syntax = syntax;
 	this.desc = desc;
-	this.run = function() {
-		console.log('EXECUTED ' + this.nm + 'IN DEFAULT MODE');
+	this.run = function (args) {
+		console.log('EXECUTED ' + this.nm + ' IN DEFAULT MODE');
 	};
 }
 
+var say = new cmd('say', '[msg]', 'A command that makes me say something.');
+say.run = function(args) {
+	console.log(args[1] + "OMG");
+}
+
+var cmds = [say];
+
 //By: http://stackoverflow.com/questions/646628/how-to-check-if-a-string-startswith-another-string
-function stringStartsWith (string, prefix) {
+function stringStartsWith(string, prefix) {
 	return string.slice(0, prefix.length) == prefix;
 }
 
-function parseCmd (msg) {
-	
+function parseCmd(msg) {
+	msg = msg.slice(1, msg.length);
+	args = msg.split(' ');
+	for (var i = 0; i < cmds.length; i++) {
+		if(args[0] == cmds[i].nm){
+			cmds[i].run(args);
+		};
+	};
 }
-
 
 $(document).bind('DOMNodeInserted', function(e) {
 	msgText = $('#main > div > div.pane-chat-msgs.pane-chat-body.lastTabIndex > div.message-list > div:last > div > div > div.message-text > span.emojitext.selectable-text').html();
 	if (lastText != msgText){
-		console.log(msgText);
 		if (stringStartsWith(msgText, '#'))
 			parseCmd(msgText);
 	}
